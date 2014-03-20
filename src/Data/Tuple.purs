abd77b8c5b8381b0ee888cbe0830354199c874b6
joneshf <jones3.hardy@gmail.com>
2014-03-20 10:36:12 -0700

    Added swap.

--- src/Data/Tuple.purs
+++ src/Data/Tuple.purs
@@ -5,14 +5,21 @@ import Data.Array
 
 data Tuple a b = Tuple a b
 
+instance showTuple :: (Show a, Show b) => Show (Tuple a b) where
+  show (Tuple a b) = "Tuple(" ++ show a ++ ", " ++ show b ++ ")"
+
+instance eqTuple :: (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple a1 b1) (Tuple a2 b2) = a1 == a2 && b1 == b2
+  (/=) t1 t2 = not (t1 == t2)
+
+instance functorTuple :: Functor (Tuple a) where
+  (<$>) f (Tuple x y) = Tuple x (f y)
+
 fst :: forall a b. Tuple a b -> a
 fst (Tuple a _) = a
 
 snd :: forall a b. Tuple a b -> b
 snd (Tuple _ b) = b
-
-instance showTuple :: (Show a, Show b) => Show (Tuple a b) where
-  show (Tuple a b) = "Tuple(" ++ show a ++ ", " ++ show b ++ ")"
 
 curry :: forall a b c. (Tuple a b -> c) -> a -> b -> c
 curry f a b = f (Tuple a b)
@@ -28,9 +35,5 @@ unzip ((Tuple a b):ts) = case unzip ts of
   Tuple as bs -> Tuple (a : as) (b : bs)
 unzip [] = Tuple [] []
 
-instance eqTuple :: (Eq a, Eq b) => Eq (Tuple a b) where
-  (==) (Tuple a1 b1) (Tuple a2 b2) = a1 == a2 && b1 == b2
-  (/=) t1 t2 = not (t1 == t2)
-
-instance functorTuple :: Functor (Tuple a) where
-  (<$>) f (Tuple x y) = Tuple x (f y)
+swap :: forall a b. Tuple a b -> Tuple b a
+swap (Tuple a b) = Tuple b a
