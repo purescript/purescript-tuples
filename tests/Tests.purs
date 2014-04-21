fed3f6fb23630f7ef77b22c63ee5a6aed67fe905
Gary Burgess <gary.burgess@gmail.com>
2014-04-21 12:11:46 +0100

    More tests

--- tests/Tests.purs
+++ tests/Tests.purs
@@ -9,40 +9,44 @@ import Test.QuickCheck
 import Test.QuickCheck.Functions
 import Test.QuickCheck.Classes
 
+type Ty = Tuple [Number] [Number]
+
 main = do
 
-  let ty = Tuple 0 1
+  let ty = Tuple [0] [1]
 
   trace "test equality"
-  check $ \x y -> Tuple x y == Tuple x y
+  check2 $ \x y -> Tuple x y == Tuple x y
   
   trace "test inequality"
-  check $ \x y -> not $ Tuple x y == Tuple (x + 1) y
+  check2 $ \x y -> not $ Tuple x y == Tuple (x + 1) y
   
   trace "test ordering on the first element"
-  let testOrderFst :: Number -> Number -> Number -> Boolean
-      testOrderFst a b x = compare (Tuple a x) (Tuple b x) == compare a b
-  quickCheck testOrderFst
+  check3 \a b x -> compare (Tuple a x) (Tuple b x) == compare a b
   
   trace "test ordering on the second element (when first element is equal)"
-  let testOrderSnd :: Number -> Number -> Number -> Boolean
-      testOrderSnd a b x = compare (Tuple x a) (Tuple x b) == compare a b
-  quickCheck testOrderSnd
+  check3 \a b x -> compare (Tuple x a) (Tuple x b) == compare a b
   
   trace "test functor laws"
   checkFunctor ty
+
+  trace "test applicative laws"
+  checkApplicative ty ty ty
+
+  trace "test monad laws"
+  checkMonad ty
   
   trace "fst should return the first element"
-  check $ \x y -> fst (Tuple x y) == x
+  check2 $ \x y -> fst (Tuple x y) == x
   
   trace "snd should return the second element"
-  check $ \x y -> snd (Tuple x y) == y
+  check2 $ \x y -> snd (Tuple x y) == y
   
   trace "curry should maintain argument order"
-  check $ \x y -> curry (\(Tuple x y) -> x / y) x y == x / y
+  check2 $ \x y -> curry (\(Tuple x y) -> x / y) x y == x / y
   
   trace "uncurry should maintain argument order"
-  check $ \x y -> uncurry (\x y -> x / y) (Tuple x y) == x / y
+  check2 $ \x y -> uncurry (\x y -> x / y) (Tuple x y) == x / y
   
   trace "zip should produce a list that matches the length of the shorter input"
   let testZipSize :: [Number] -> [Number] -> Boolean
@@ -67,10 +71,22 @@ main = do
   quickCheck testUnzipPairs
   
   trace "swap should switch the first and second element"
-  check $ \x y -> swap (Tuple x y) == (Tuple y x)
-  
-check :: (Number -> Number -> Boolean) -> QC
-check = quickCheck
+  check2 $ \x y -> swap (Tuple x y) == (Tuple y x)
+
+check2 :: (Number -> Number -> Boolean) -> QC
+check2 = quickCheck
+
+check3 :: (Number -> Number -> Number -> Boolean) -> QC
+check3 = quickCheck
+
+instance arbAToTupleBC :: (Arb (a -> b), Arb (a -> c)) => Arb (a -> Tuple b c) where
+  arb = do
+    f <- arb
+    g <- arb
+    return $ \x -> Tuple (f x) (g x)
+
+instance showAToTupleBC :: (Arb (a -> b), Arb (a -> c)) => Show (a -> Tuple b c) where
+  show _ = "<a -> Tuple b c>"
 
 compareZip :: forall a b. (Eq a, Eq b) => [a] -> [b] -> [Tuple a b] -> Boolean
 compareZip (x : xs) (y : ys) ((Tuple x' y') : ts) = x == x' && y == y' && compareZip xs ys ts
