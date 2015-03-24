c115490f8e33ea409df056f304c9e17e71d0afd4
John A. De Goes <john@degoes.net>
2015-03-24 13:34:18 -0600

    cleaner nested tuple machinery

--- src/Data/Tuple.purs
+++ src/Data/Tuple.purs
@@ -125,41 +125,51 @@ module Data.Tuple where
 module Data.Tuple.Nested where
   import Data.Tuple
 
+  type Tuple2 a b = Tuple a b
+  type Tuple3 a b c = Tuple a (Tuple2 b c)
+  type Tuple4 a b c d = Tuple a (Tuple3 b c d)
+  type Tuple5 a b c d e = Tuple a (Tuple4 b c d e)
+  type Tuple6 a b c d e f = Tuple a (Tuple5 b c d e f)
+  type Tuple7 a b c d e f g = Tuple a (Tuple6 b c d e f g)
+  type Tuple8 a b c d e f g h = Tuple a (Tuple7 b c d e f g h)
+  type Tuple9 a b c d e f g h i = Tuple a (Tuple8 b c d e f g h i)
+  type Tuple10 a b c d e f g h i j = Tuple a (Tuple9 b c d e f g h i j)
+  
   -- | Given a function of 2 arguments, return a function that accepts a 2-tuple.
-  con2 :: forall a b z. (a -> b -> z) -> (Tuple a b) -> z
-  con2 f = \(Tuple a b) -> f a b
+  tuple2 :: forall a b z. (a -> b -> z) -> Tuple2 a b -> z
+  tuple2 f = \(Tuple a b) -> f a b
 
   -- | Given a function of 3 arguments, return a function that accepts a 3-tuple.
-  con3 :: forall a b c z. (a -> b -> c -> z) -> (Tuple a (Tuple b c)) -> z
-  con3 f = \(Tuple a (Tuple b c)) -> f a b c
+  tuple3 :: forall a b c z. (a -> b -> c -> z) -> Tuple3 a b c -> z
+  tuple3 f = \(Tuple a (Tuple b c)) -> f a b c
 
   -- | Given a function of 4 arguments, return a function that accepts a 4-tuple.
-  con4 :: forall a b c d z. (a -> b -> c -> d -> z) -> (Tuple a (Tuple b (Tuple c d))) -> z
-  con4 f = \(Tuple a (Tuple b (Tuple c d))) -> f a b c d
+  tuple4 :: forall a b c d z. (a -> b -> c -> d -> z) -> Tuple4 a b c d -> z
+  tuple4 f = \(Tuple a (Tuple b (Tuple c d))) -> f a b c d
 
   -- | Given a function of 5 arguments, return a function that accepts a 5-tuple.
-  con5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d e)))) -> z
-  con5 f = \(Tuple a (Tuple b (Tuple c (Tuple d e)))) -> f a b c d e
+  tuple5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> Tuple5 a b c d e -> z
+  tuple5 f = \(Tuple a (Tuple b (Tuple c (Tuple d e)))) -> f a b c d e
 
   -- | Given a function of 6 arguments, return a function that accepts a 6-tuple.
-  con6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d (Tuple e f))))) -> z
-  con6 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e f'))))) -> f a b c d e f'
+  tuple6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> Tuple6 a b c d e f -> z
+  tuple6 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e f'))))) -> f a b c d e f'
 
   -- | Given a function of 7 arguments, return a function that accepts a 7-tuple.
-  con7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f g)))))) -> z
-  con7 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' g)))))) -> f a b c d e f' g
+  tuple7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> Tuple7 a b c d e f g -> z
+  tuple7 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' g)))))) -> f a b c d e f' g
 
   -- | Given a function of 8 arguments, return a function that accepts a 8-tuple.
-  con8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f (Tuple g h))))))) -> z
-  con8 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g h))))))) -> f a b c d e f' g h
+  tuple8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> Tuple8 a b c d e f g h -> z
+  tuple8 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g h))))))) -> f a b c d e f' g h
 
   -- | Given a function of 9 arguments, return a function that accepts a 9-tuple.
-  con9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f (Tuple g (Tuple h i)))))))) -> z
-  con9 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g (Tuple h i)))))))) -> f a b c d e f' g h i
+  tuple9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> Tuple9 a b c d e f g h i -> z
+  tuple9 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g (Tuple h i)))))))) -> f a b c d e f' g h i
 
   -- | Given a function of 10 arguments, return a function that accepts a 10-tuple.
-  con10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> (Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f (Tuple g (Tuple h (Tuple i j))))))))) -> z
-  con10 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g (Tuple h (Tuple i j))))))))) -> f a b c d e f' g h i j
+  tuple10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> Tuple10 a b c d e f g h i j -> z
+  tuple10 f = \(Tuple a (Tuple b (Tuple c (Tuple d (Tuple e (Tuple f' (Tuple g (Tuple h (Tuple i j))))))))) -> f a b c d e f' g h i j
 
   infixr 6 /\
 
