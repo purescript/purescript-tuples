d1f923bf1e1e903f6fe020ee959ffb5b1608931c
John A. De Goes <john@degoes.net>
2015-03-25 11:15:06 -0600

    break all the things

--- README.md
+++ README.md
@@ -345,146 +345,146 @@ entuple10 :: forall a b c d e f g h i j. a -> b -> c -> d -> e -> f -> g -> h ->
 
 Given 10 values, creates a nested 10-tuple.
 
-#### `tuple2`
+#### `uncurry2`
 
 ``` purescript
-tuple2 :: forall a b z. (a -> b -> z) -> Tuple2 a b -> z
+uncurry2 :: forall a b z. (a -> b -> z) -> Tuple2 a b -> z
 ```
 
 Given a function of 2 arguments, return a function that accepts a 2-tuple.
 
-#### `unTuple2`
+#### `curry2`
 
 ``` purescript
-unTuple2 :: forall a b z. (Tuple2 a b -> z) -> a -> b -> z
+curry2 :: forall a b z. (Tuple2 a b -> z) -> a -> b -> z
 ```
 
 Given a function that accepts a 2-tuple, return a function of 2 arguments.
 
-#### `tuple3`
+#### `uncurry3`
 
 ``` purescript
-tuple3 :: forall a b c z. (a -> b -> c -> z) -> Tuple3 a b c -> z
+uncurry3 :: forall a b c z. (a -> b -> c -> z) -> Tuple3 a b c -> z
 ```
 
 Given a function of 3 arguments, return a function that accepts a 3-tuple.
 
-#### `unTuple3`
+#### `curry3`
 
 ``` purescript
-unTuple3 :: forall a b c z. (Tuple3 a b c -> z) -> a -> b -> c -> z
+curry3 :: forall a b c z. (Tuple3 a b c -> z) -> a -> b -> c -> z
 ```
 
 Given a function that accepts a 3-tuple, return a function of 3 arguments.
 
-#### `tuple4`
+#### `uncurry4`
 
 ``` purescript
-tuple4 :: forall a b c d z. (a -> b -> c -> d -> z) -> Tuple4 a b c d -> z
+uncurry4 :: forall a b c d z. (a -> b -> c -> d -> z) -> Tuple4 a b c d -> z
 ```
 
 Given a function of 4 arguments, return a function that accepts a 4-tuple.
 
-#### `unTuple4`
+#### `curry4`
 
 ``` purescript
-unTuple4 :: forall a b c d z. (Tuple4 a b c d -> z) -> a -> b -> c -> d -> z
+curry4 :: forall a b c d z. (Tuple4 a b c d -> z) -> a -> b -> c -> d -> z
 ```
 
 Given a function that accepts a 4-tuple, return a function of 4 arguments.
 
-#### `tuple5`
+#### `uncurry5`
 
 ``` purescript
-tuple5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> Tuple5 a b c d e -> z
+uncurry5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> Tuple5 a b c d e -> z
 ```
 
 Given a function of 5 arguments, return a function that accepts a 5-tuple.
 
-#### `unTuple5`
+#### `curry5`
 
 ``` purescript
-unTuple5 :: forall a b c d e z. (Tuple5 a b c d e -> z) -> a -> b -> c -> d -> e -> z
+curry5 :: forall a b c d e z. (Tuple5 a b c d e -> z) -> a -> b -> c -> d -> e -> z
 ```
 
 Given a function that accepts a 5-tuple, return a function of 5 arguments.
 
-#### `tuple6`
+#### `uncurry6`
 
 ``` purescript
-tuple6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> Tuple6 a b c d e f -> z
+uncurry6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> Tuple6 a b c d e f -> z
 ```
 
 Given a function of 6 arguments, return a function that accepts a 6-tuple.
 
-#### `unTuple6`
+#### `curry6`
 
 ``` purescript
-unTuple6 :: forall a b c d e f z. (Tuple6 a b c d e f -> z) -> a -> b -> c -> d -> e -> f -> z
+curry6 :: forall a b c d e f z. (Tuple6 a b c d e f -> z) -> a -> b -> c -> d -> e -> f -> z
 ```
 
 Given a function that accepts a 6-tuple, return a function of 6 arguments.
 
-#### `tuple7`
+#### `uncurry7`
 
 ``` purescript
-tuple7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> Tuple7 a b c d e f g -> z
+uncurry7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> Tuple7 a b c d e f g -> z
 ```
 
 Given a function of 7 arguments, return a function that accepts a 7-tuple.
 
-#### `unTuple7`
+#### `curry7`
 
 ``` purescript
-unTuple7 :: forall a b c d e f g z. (Tuple7 a b c d e f g -> z) -> a -> b -> c -> d -> e -> f -> g -> z
+curry7 :: forall a b c d e f g z. (Tuple7 a b c d e f g -> z) -> a -> b -> c -> d -> e -> f -> g -> z
 ```
 
 Given a function that accepts a 7-tuple, return a function of 7 arguments.
 
-#### `tuple8`
+#### `uncurry8`
 
 ``` purescript
-tuple8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> Tuple8 a b c d e f g h -> z
+uncurry8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> Tuple8 a b c d e f g h -> z
 ```
 
 Given a function of 8 arguments, return a function that accepts a 8-tuple.
 
-#### `unTuple8`
+#### `curry8`
 
 ``` purescript
-unTuple8 :: forall a b c d e f g h z. (Tuple8 a b c d e f g h -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> z
+curry8 :: forall a b c d e f g h z. (Tuple8 a b c d e f g h -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> z
 ```
 
 Given a function that accepts a 8-tuple, return a function of 8 arguments.
 
-#### `tuple9`
+#### `uncurry9`
 
 ``` purescript
-tuple9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> Tuple9 a b c d e f g h i -> z
+uncurry9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> Tuple9 a b c d e f g h i -> z
 ```
 
 Given a function of 9 arguments, return a function that accepts a 9-tuple.
 
-#### `unTuple9`
+#### `curry9`
 
 ``` purescript
-unTuple9 :: forall a b c d e f g h i z. (Tuple9 a b c d e f g h i -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> z
+curry9 :: forall a b c d e f g h i z. (Tuple9 a b c d e f g h i -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> z
 ```
 
 Given a function that accepts a 9-tuple, return a function of 9 arguments.
 
-#### `tuple10`
+#### `uncurry10`
 
 ``` purescript
-tuple10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> Tuple10 a b c d e f g h i j -> z
+uncurry10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> Tuple10 a b c d e f g h i j -> z
 ```
 
 Given a function of 10 arguments, return a function that accepts a 10-tuple.
 
-#### `unTuple10`
+#### `curry10`
 
 ``` purescript
-unTuple10 :: forall a b c d e f g h i j z. (Tuple10 a b c d e f g h i j -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z
+curry10 :: forall a b c d e f g h i j z. (Tuple10 a b c d e f g h i j -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z
 ```
 
 Given a function that accepts a 10-tuple, return a function of 10 arguments.
--- src/Data/Tuple.purs
+++ src/Data/Tuple.purs
@@ -135,10 +135,10 @@ module Data.Tuple where
 -- | exampleAddress2 = makeAddressT $ "221B Baker Street" /\ London /\ Nothing /\ UK
 -- | 
 -- | makeAddressT :: Tuple4 String City (Maybe Province) Country -> Address
--- | makeAddressT = tuple4 Address
+-- | makeAddressT = uncurry4 Address
 -- |
 -- | makeAddress :: String -> City -> (Maybe Province) -> Country -> Address
--- | makeAddress = unTuple4 makeAddressT
+-- | makeAddress = curry4 makeAddressT
 -- | 
 -- | tupleAddress :: Address -> Tuple4 String City (Maybe Province) Country
 -- | tupleAddress (Address a b c d) = entuple4 a b c d
@@ -193,76 +193,76 @@ module Data.Tuple.Nested where
   entuple10 a b c d e f g h i j = entuple2 (entuple9 a b c d e f g h i) j
   
   -- | Given a function of 2 arguments, return a function that accepts a 2-tuple.
-  tuple2 :: forall a b z. (a -> b -> z) -> Tuple2 a b -> z
-  tuple2 f = \(Tuple t z) -> f t z
+  uncurry2 :: forall a b z. (a -> b -> z) -> Tuple2 a b -> z
+  uncurry2 f = \(Tuple t z) -> f t z
 
   -- | Given a function that accepts a 2-tuple, return a function of 2 arguments.
-  unTuple2 :: forall a b z. (Tuple2 a b -> z) -> a -> b -> z
-  unTuple2 f a b = f (entuple2 a b)
+  curry2 :: forall a b z. (Tuple2 a b -> z) -> a -> b -> z
+  curry2 f a b = f (entuple2 a b)
 
   -- | Given a function of 3 arguments, return a function that accepts a 3-tuple.
-  tuple3 :: forall a b c z. (a -> b -> c -> z) -> Tuple3 a b c -> z
-  tuple3 f = \(Tuple t z) -> tuple2 f t z
+  uncurry3 :: forall a b c z. (a -> b -> c -> z) -> Tuple3 a b c -> z
+  uncurry3 f = \(Tuple t z) -> uncurry2 f t z
 
   -- | Given a function that accepts a 3-tuple, return a function of 3 arguments.
-  unTuple3 :: forall a b c z. (Tuple3 a b c -> z) -> a -> b -> c -> z
-  unTuple3 f a b c = f (entuple3 a b c)
+  curry3 :: forall a b c z. (Tuple3 a b c -> z) -> a -> b -> c -> z
+  curry3 f a b c = f (entuple3 a b c)
 
   -- | Given a function of 4 arguments, return a function that accepts a 4-tuple.
-  tuple4 :: forall a b c d z. (a -> b -> c -> d -> z) -> Tuple4 a b c d -> z
-  tuple4 f = \(Tuple t z) -> tuple3 f t z
+  uncurry4 :: forall a b c d z. (a -> b -> c -> d -> z) -> Tuple4 a b c d -> z
+  uncurry4 f = \(Tuple t z) -> uncurry3 f t z
 
   -- | Given a function that accepts a 4-tuple, return a function of 4 arguments.
-  unTuple4 :: forall a b c d z. (Tuple4 a b c d -> z) -> a -> b -> c -> d -> z
-  unTuple4 f a b c d = f (entuple4 a b c d)
+  curry4 :: forall a b c d z. (Tuple4 a b c d -> z) -> a -> b -> c -> d -> z
+  curry4 f a b c d = f (entuple4 a b c d)
 
   -- | Given a function of 5 arguments, return a function that accepts a 5-tuple.
-  tuple5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> Tuple5 a b c d e -> z
-  tuple5 f = \(Tuple t z) -> tuple4 f t z
+  uncurry5 :: forall a b c d e z. (a -> b -> c -> d -> e -> z) -> Tuple5 a b c d e -> z
+  uncurry5 f = \(Tuple t z) -> uncurry4 f t z
 
   -- | Given a function that accepts a 5-tuple, return a function of 5 arguments.
-  unTuple5 :: forall a b c d e z. (Tuple5 a b c d e -> z) -> a -> b -> c -> d -> e -> z
-  unTuple5 f a b c d e = f (entuple5 a b c d e)
+  curry5 :: forall a b c d e z. (Tuple5 a b c d e -> z) -> a -> b -> c -> d -> e -> z
+  curry5 f a b c d e = f (entuple5 a b c d e)
 
   -- | Given a function of 6 arguments, return a function that accepts a 6-tuple.
-  tuple6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> Tuple6 a b c d e f -> z
-  tuple6 f = \(Tuple t z) -> tuple5 f t z
+  uncurry6 :: forall a b c d e f z. (a -> b -> c -> d -> e -> f -> z) -> Tuple6 a b c d e f -> z
+  uncurry6 f = \(Tuple t z) -> uncurry5 f t z
 
   -- | Given a function that accepts a 6-tuple, return a function of 6 arguments.
-  unTuple6 :: forall a b c d e f z. (Tuple6 a b c d e f -> z) -> a -> b -> c -> d -> e -> f -> z
-  unTuple6 f' a b c d e f = f' (entuple6 a b c d e f)
+  curry6 :: forall a b c d e f z. (Tuple6 a b c d e f -> z) -> a -> b -> c -> d -> e -> f -> z
+  curry6 f' a b c d e f = f' (entuple6 a b c d e f)
 
   -- | Given a function of 7 arguments, return a function that accepts a 7-tuple.
-  tuple7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> Tuple7 a b c d e f g -> z
-  tuple7 f = \(Tuple t z) -> tuple6 f t z
+  uncurry7 :: forall a b c d e f g z. (a -> b -> c -> d -> e -> f -> g -> z) -> Tuple7 a b c d e f g -> z
+  uncurry7 f = \(Tuple t z) -> uncurry6 f t z
 
   -- | Given a function that accepts a 7-tuple, return a function of 7 arguments.
-  unTuple7 :: forall a b c d e f g z. (Tuple7 a b c d e f g -> z) -> a -> b -> c -> d -> e -> f -> g -> z
-  unTuple7 f' a b c d e f g = f' (entuple7 a b c d e f g)
+  curry7 :: forall a b c d e f g z. (Tuple7 a b c d e f g -> z) -> a -> b -> c -> d -> e -> f -> g -> z
+  curry7 f' a b c d e f g = f' (entuple7 a b c d e f g)
 
   -- | Given a function of 8 arguments, return a function that accepts a 8-tuple.
-  tuple8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> Tuple8 a b c d e f g h -> z
-  tuple8 f = \(Tuple t z) -> tuple7 f t z
+  uncurry8 :: forall a b c d e f g h z. (a -> b -> c -> d -> e -> f -> g -> h -> z) -> Tuple8 a b c d e f g h -> z
+  uncurry8 f = \(Tuple t z) -> uncurry7 f t z
 
   -- | Given a function that accepts a 8-tuple, return a function of 8 arguments.
-  unTuple8 :: forall a b c d e f g h z. (Tuple8 a b c d e f g h -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> z
-  unTuple8 f' a b c d e f g h = f' (entuple8 a b c d e f g h)
+  curry8 :: forall a b c d e f g h z. (Tuple8 a b c d e f g h -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> z
+  curry8 f' a b c d e f g h = f' (entuple8 a b c d e f g h)
 
   -- | Given a function of 9 arguments, return a function that accepts a 9-tuple.
-  tuple9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> Tuple9 a b c d e f g h i -> z
-  tuple9 f = \(Tuple t z) -> tuple8 f t z
+  uncurry9 :: forall a b c d e f g h i z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> z) -> Tuple9 a b c d e f g h i -> z
+  uncurry9 f = \(Tuple t z) -> uncurry8 f t z
 
   -- | Given a function that accepts a 9-tuple, return a function of 9 arguments.
-  unTuple9 :: forall a b c d e f g h i z. (Tuple9 a b c d e f g h i -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> z
-  unTuple9 f' a b c d e f g h i = f' (entuple9 a b c d e f g h i)
+  curry9 :: forall a b c d e f g h i z. (Tuple9 a b c d e f g h i -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> z
+  curry9 f' a b c d e f g h i = f' (entuple9 a b c d e f g h i)
 
   -- | Given a function of 10 arguments, return a function that accepts a 10-tuple.
-  tuple10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> Tuple10 a b c d e f g h i j -> z
-  tuple10 f = \(Tuple t z) -> tuple9 f t z
+  uncurry10 :: forall a b c d e f g h i j z. (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z) -> Tuple10 a b c d e f g h i j -> z
+  uncurry10 f = \(Tuple t z) -> uncurry9 f t z
 
   -- | Given a function that accepts a 10-tuple, return a function of 10 arguments.
-  unTuple10 :: forall a b c d e f g h i j z. (Tuple10 a b c d e f g h i j -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z
-  unTuple10 f' a b c d e f g h i j = f' (entuple10 a b c d e f g h i j)
+  curry10 :: forall a b c d e f g h i j z. (Tuple10 a b c d e f g h i j -> z) -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> z
+  curry10 f' a b c d e f g h i j = f' (entuple10 a b c d e f g h i j)
 
   infixl 6 /\
 
